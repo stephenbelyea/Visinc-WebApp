11a882d1226384aa7feff6a80ac1b33a12b00565
stephenbelyea <belyea.stephen@gmail.com>
2013-06-26 10:35:19 -0400

    Remove GitIgnore
    Config file only showing login for local server.

---
+++ includes/config.php
@@ -0,0 +1,8 @@
+<?php 
+
+	define('KEY_GOOGLE', 	'AIzaSyD6jQSuPRoe1lBfvnsUPFd6SPfJPNZqAmY');
+	define('KEY_FLICKR', 	'dcb0965d99785f472627669e1500191e');
+	define('FLICKR_ID', 	'98422522@N00');
+	//define('FLICKR_USER', 	'adamschoales');
+	define('FLICKR_USER', 	'adamschoales');
+
